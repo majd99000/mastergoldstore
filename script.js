2dad1fa7d16897ac3fa6ea84239d5e2f845e6192
majd99000 <majd.nashed@gmail.com>
2025-04-08 15:09:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.product button').forEach(button => {
+document.querySelectorAll('.buy-btn').forEach(button => {
     button.addEventListener('click', () => {
         alert('تم إضافة المنتج إلى سلة الشراء!');
     });
